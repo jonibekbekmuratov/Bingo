4c3b68f6e723f871ab0b1e9151191dbb33bf6912
Jonibek <jonibekbekmuratov99@gmail.com>
2023-08-15 10:57:26 +0500

    Initial Commit

---
+++ Bingo/BingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingoApp.swift
+//  Bingo
+//
+//  Created by Johny on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
